03ecf62630ebd7162674cf35a81b564fd741998c
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-04 16:56:01 -0400

    Show alert of error

--- src/app/components/FormCreateUser.jsx
+++ src/app/components/FormCreateUser.jsx
@@ -2,20 +2,51 @@
 import Link from "next/link";
 import useValidation from "@/app/hooks/useValidation";
 import validationCreateUser from "@/app/validation/validationCreateUser";
+import { toast } from "react-toastify";
 
 const FormCreateUser = () => {
     const createAccount = () => {
         console.log("Creando cuenta");
     };
 
-    const { value, error, submitForm, handleChange, handleSubmit } =
+    const { value, error, submitForm, handleChange, handleSubmit, handleBlur } =
         useValidation(validationCreateUser, createAccount);
 
     return (
-        <form className="space-y-6" onSubmit={handleSubmit}>
+        <form className="space-y-5" onSubmit={handleSubmit}>
             <h5 className="text-xl font-medium text-gray-900 dark:text-white">
                 Registrarse
             </h5>
+            {Object.keys(error).length !== 0 ? (
+                <div
+                    className="flex p-3 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-900 dark:text-red-400"
+                >
+                    <svg
+                        aria-hidden="true"
+                        className="flex-shrink-0 inline w-5 h-5 mr-3"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                            clip-rule="evenodd"
+                        ></path>
+                    </svg>
+                    <span className="sr-only">Danger</span>
+                    <div>
+                        <span className="font-medium">
+                            Debe corregir lo siguiente:
+                        </span>
+                        <ul className="mt-1.5 ml-4 list-disc list-inside">
+                            {error.name && <li>{error.name}</li>}
+                            {error.email && <li>{error.email}</li>}
+                            {error.password && <li>{error.password}</li>}
+                        </ul>
+                    </div>
+                </div>
+            ) : null}
             <div>
                 <label
                     htmlFor="name"
@@ -30,6 +61,7 @@ const FormCreateUser = () => {
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white outline-none transition duration-150 hover:border-blue-400 focus:border-blue-700"
                     placeholder="Nombre@dominio.com"
                     onChange={(e) => handleChange(e)}
+                    onBlur={handleBlur}
                     autoComplete="off"
                 />
             </div>
@@ -47,6 +79,7 @@ const FormCreateUser = () => {
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white outline-none transition duration-150 hover:border-blue-400 focus:border-blue-700"
                     placeholder="Nombre@dominio.com"
                     onChange={(e) => handleChange(e)}
+                    onBlur={handleBlur}
                     autoComplete="off"
                 />
             </div>
@@ -64,6 +97,7 @@ const FormCreateUser = () => {
                     placeholder="••••••••"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white outline-none transition duration-150 hover:border-blue-400 focus:border-blue-700"
                     onChange={(e) => handleChange(e)}
+                    onBlur={handleBlur}
                 />
             </div>
             <button
