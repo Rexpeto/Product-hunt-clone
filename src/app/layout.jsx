14abbb8e3a423bb9b89920fb02d93d9fa65b3694
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-05 13:17:14 -0400

    Use provider firebase

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import Header from "@/app/components/Header";
 import "@/app/globals.css";
 import { Suspense } from "react";
 import Loading from "./loading";
+import { FirebaseProvider } from "./firebase/context";
 
 export const metadata = {
     title: "Product Hunt Clone",
@@ -15,12 +16,14 @@ export default function RootLayout({ children }) {
     return (
         <html lang="es">
             <body className="bg-white dark:bg-gray-900 h-screen">
-                <Header />
-                <Suspense fallback={<Loading />}>
-                    <main className="container mx-auto dark:text-white pt-4">
-                        {children}
-                    </main>
-                </Suspense>
+                <FirebaseProvider>
+                    <Header />
+                    <Suspense fallback={<Loading />}>
+                        <main className="container mx-auto dark:text-white pt-4">
+                            {children}
+                        </main>
+                    </Suspense>
+                </FirebaseProvider>
             </body>
         </html>
     );
